a1c95f24e8fe9ef5c9e3558eedad1c0b3e428d44
joaquingz <joaquin.gzm234@gmail.com>
2025-05-25 20:13:50 -0300

    haciendo otro parcial

---
+++ PracticaParciales/parcial6.pas
@@ -0,0 +1,64 @@
+{
+Asumo que la información en el txt va a estar ordenada como:
+
+codigoExcursion CantidadTotalVendida
+nombre
+descripcion
+
+}
+
+program parcial6;
+
+
+type
+
+    regisotroMaestro = record
+        codigoExcursion : integer;
+        cantidadTotalVendida : integer;
+        nombre : string;
+        descripcion : string;
+    end;
+
+    maestro = file of registroMaestro;
+
+
+var
+
+    t : text;
+    m : maestro;
+    aux,rM : registroMaestro;
+    codActual : integer;
+
+begin
+
+    assign(t,'detalle.txt');
+    assign(m,'maestro.dat');
+    rewrite(m);
+
+    readln(t,aux.codigoExcursion,aux.cantidadTotalVendida);
+    readln(t,aux.nombre);
+    readln(t,aux.descripcion);
+    while(not eol(t))do
+    begin
+
+        rM := aux;
+        rM.cantidadTotalVendida := 0;
+        codActual := aux.codigoExcursion;
+
+        while(not eol(t))and(codActual = aux.codigoExcursion)do
+        begin
+
+            rM.cantidadTotalVendida := rM.cantidadTotalVentida + aux.cantidadTotalVendida;
+            readln(t,aux.codigoExcursion,aux.cantidadTotalVendida);
+            readln(t,aux.nombre);
+            readln(t,aux.descripcion);    
+
+        end;
+
+        write(m,rM);
+    end;
+
+    close(t);
+    close(m);
+
+end.
